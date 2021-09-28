f42b40bf6fdb6a5a26f9387c6cda1c7332fdd3e1
Drakenwolf <hans.haar6@gmail.com>
2021-09-28 09:52:13 -0700

    Memoiztion practice

---
+++ src/List/List.tsx
@@ -0,0 +1,18 @@
+import { FC, useEffect, memo } from 'react'
+import Task from '../Task/Task'
+
+export type Todo = { id: number,  task: string }
+interface Props { todoList: Todo[] }
+
+const List: FC<Props> = ({ todoList }) => {
+    useEffect(() => {
+        // This effect is executed every new render
+        console.log('Rendering <List />')
+    })
+    return (
+        <ul>
+            {todoList.map((todo: Todo) => (<Task key={todo.id} id={todo.id} task={todo.task} />))}
+        </ul>
+    )
+}
+export default memo (List)
